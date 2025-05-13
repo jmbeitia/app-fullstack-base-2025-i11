4b8911cd71291365b394d592a14637b6c1199649
Your Name <you@example.com>
2025-05-12 21:28:10 -0300

    manejo de clases

---
+++ src/frontend/ts/usuario.ts
@@ -0,0 +1,15 @@
+class Usuario extends Persona{
+  private mail: string;
+  constructor(nombre:string,dni:number,mail: string) { 
+    super(nombre, dni);
+    this.mail = mail;
+  }
+
+  public obtenerDatos():string {
+    return this.mail + "-" + super.obtenerDatos();
+    
+  }
+  public verMail(): string{
+    return this.mail;
+  }
+}
